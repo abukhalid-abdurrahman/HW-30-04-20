6645c2463fcee8a62064277d658b5298c152659d
faha-berdiev <madysoftware@gmail.com>
2020-05-02 19:23:15 +0300

    Поменял алгоритм работы задачи

--- Program.cs
+++ Program.cs
@@ -1,66 +1,90 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace Day_16
 {
     class Program
     {
-        static int curLeft;
-        static int curTop;
-        static ConsoleColor firstChrColor = ConsoleColor.White;
-        static ConsoleColor secondChrColor = ConsoleColor.Green;
-        static ConsoleColor otherChrColor = ConsoleColor.DarkGreen;
-        static string symbols = "QWERTYUIOPASDFGHJKLZXCVBNM1234567890";
-        static void OutChain(int indexChr, int x, int y)
+        struct Chain
         {
-            Task taskChangeSymbol = new Task(() =>
+            public string Symbols;
+            public int X;
+            public int Y;
+        };
+        static int cursorLeft;
+        static int cursorTop;
+        static char GenerateChainSymbol()
+        {
+            string symbolsChain = "QWERTYUIOPASDFGHJKLZXCVBNM1234567890";
+            return symbolsChain[new Random().Next(0, 35)];
+        }
+        static List<Chain> chainsList = new List<Chain>();
+        // static void OutChain(int indexChr, int x)
+        // {
+        //     Task taskChangeSymbol = new Task(() =>
+        //     {
+        //         Console.ResetColor();
+        //         Console.ForegroundColor = otherChrColor;
+        //         Console.SetCursorPosition(curLeft + x, 1);
+        //         Console.Write(symbols[indexChr + 2]);
+        //         Thread.Sleep(50);
+        //         Console.SetCursorPosition(curLeft + x, 2);
+        //         Console.Write(symbols[indexChr + 3]);
+        //         Thread.Sleep(50);
+        //         Console.SetCursorPosition(curLeft + x, 3);
+        //         Console.Write(symbols[indexChr + 4]);
+        //         Thread.Sleep(50);
+        //         Console.ForegroundColor = secondChrColor;
+        //         Console.SetCursorPosition(curLeft + x, 4);
+        //         Console.Write(symbols[indexChr + 5]);
+        //         Thread.Sleep(50);
+        //         Console.ForegroundColor = firstChrColor;
+        //         Console.SetCursorPosition(curLeft + x, 5);
+        //         Console.Write(symbols[indexChr + 6]);
+
+        //         Console.ResetColor();
+        //     });
+        //     taskChangeSymbol.Start();
+        // }
+        static void ChainGenerator()
+        {
+            int counter = 0;
+            object locker = new object();
+            while(counter < 10)
             {
-                Console.ResetColor();
-                Console.ForegroundColor = otherChrColor;
-                Console.SetCursorPosition(curLeft + x, curTop + y + 2);
-                Console.Write(symbols[indexChr + 2]);
+                lock (locker)
+                {
+                    Chain chain = new Chain();
+                    chain.Symbols = "";
+                    chain.X = counter + 10;
+                    chain.Y = 0;
+                    for(int i = 0; i < 10; i++)
+                    {
+                        chain.Symbols += GenerateChainSymbol();
+                    }
+                    chainsList.Add(chain);
+                    counter++;
+                }
+                
+            }
+        }
 
-                Console.SetCursorPosition(curLeft + x, curTop + y + 3);
-                Console.Write(symbols[indexChr + 3]);
-
-                Console.SetCursorPosition(curLeft + x, curTop + y + 4);
-                Console.Write(symbols[indexChr + 4]);
-
-                Console.ForegroundColor = secondChrColor;
-                Console.SetCursorPosition(curLeft + x, curTop + y + 5);
-                Console.Write(symbols[indexChr + 5]);
-
-                Console.ForegroundColor = firstChrColor;
-                Console.SetCursorPosition(curLeft + x, curTop + y + 6);
-                Console.Write(symbols[indexChr + 6]);
-
-                Console.ResetColor();
-            });
-            taskChangeSymbol.Start();
+        static void InitCursors()
+        {
+            cursorLeft = Console.CursorLeft;
+            cursorTop = Console.CursorTop;
         }
         static void Main(string[] args)
         {
-            curLeft = Console.CursorLeft;
-            curTop = Console.CursorTop;
-            Task taskChainDrawer = new Task(() =>
-            {
-                for (int i = 0; i < 10; i++)
-                {
-                    int randIndex = new Random().Next(0, symbols.Length - 6);
-                    int randX = new Random().Next(5, 100);
-                    int randY = new Random().Next(0, 5);
-                    for (int j = 0; j < 10; j++)
-                    {
-                        OutChain(randIndex, randX + i, randY + j);
-                        Thread.Sleep(50);
-                    }
-                    Thread.Sleep(100);
-                }
-            });
-            taskChainDrawer.Start();
-            taskChainDrawer.Wait();
-            Console.Read();
+            InitCursors();
+            Console.WriteLine("Task 16...");
+            Thread chainGenerator = new Thread(ChainGenerator);
+            chainGenerator.Start();
+            chainGenerator.Join();
+
+            Console.WriteLine(chainsList.Count);
+            Console.WriteLine(chainsList[4].Symbols);
         }
     }
 }
