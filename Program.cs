f204aebc18308b4b82d5b58053b6074e93e30554
faha-berdiev <madysoftware@gmail.com>
2020-05-03 15:52:16 +0300

    Конечная демо версия

--- Program.cs
+++ Program.cs
@@ -5,84 +5,49 @@ using System.Threading.Tasks;
 
 namespace Day_16
 {
-    class Chain
-    {
-        public string Symbols;
-        public int X;
-        public int Y;
-    }
     class Program
     {
-        static int cursorLeft;
-        static int cursorTop;
         static char GenerateChainSymbol()
         {
             string symbolsChain = "QWERTYUIOPASDFGHJKLZXCVBNM1234567890";
             return symbolsChain[new Random().Next(0, 35)];
         }
-        static List<Chain> chainsList = new List<Chain>();
-        static int index = 0;
         static void OutChain()
         {
             object locker = new object();
-            for (int i = 0; i < chainsList[index].Symbols.Length; i++)
+            int maxStep = 0;
+            while (maxStep < 10)
             {
-                lock (locker)
-                {
-                    Console.ResetColor();
-                    Console.SetCursorPosition(cursorLeft + chainsList[index].X, + i);
-                    Console.ForegroundColor = ConsoleColor.DarkGreen;
-                    if (i > chainsList[index].Symbols.Length - 2)
+                Console.CursorTop = maxStep;
+                //lock (locker)
+                //{
+                    for (int i = 0; i < 25; i++)
                     {
-                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.ResetColor();
+                        Console.SetCursorPosition(Console.CursorLeft, Console.CursorTop + i);
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        if (maxStep > 8)
+                        {
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        else if (maxStep > 7)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                        }
+                        Console.Write(GenerateChainSymbol());
+                        Console.ResetColor();
                     }
-                    else if (i > chainsList[index].Symbols.Length - 3)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Green;
-                    }
-                    Console.Write(chainsList[index].Symbols[i]);
-                    Console.ResetColor();
-                }
+                    maxStep++;
+                //}
             }
-        }
-        static void ChainGenerator()
-        {
-            int counter = 0;
-            object locker = new object();
-            while (counter < 25)
-            {
-                lock (locker)
-                {
-                    int symbLen = new Random().Next(4, 20);
-                    int interval = new Random().Next(3, 5);
-                    Chain chain = new Chain();
-                    chain.Symbols = "";
-                    chain.X = counter * interval;
-                    chain.Y = counter;
-                    for (int i = 0; i < symbLen; i++)
-                    {
-                        chain.Symbols += GenerateChainSymbol();
-                    }
-                    chainsList.Add(chain);
-                    counter++;
-                }
-
-            }
-        }
-        static void InitCursors()
-        {
-            cursorLeft = Console.CursorLeft;
-            cursorTop = Console.CursorTop;
         }
         static void Main(string[] args)
         {
-            InitCursors();
-            Thread chainGenerator = new Thread(ChainGenerator);
-            chainGenerator.Start();
-            chainGenerator.Join();
-            Thread[] chainsTask = new Thread[25];
-            for (int i = 0; i < chainsTask.Length; i++, index++)
+            Console.CursorTop = 0;
+            Thread[] chainsTask = new Thread[30];
+            for (int i = 0; i < chainsTask.Length; i++)
             {
+                Thread.Sleep(100);
                 chainsTask[i] = new Thread(OutChain);
                 chainsTask[i].Start();
                 chainsTask[i].Join();
